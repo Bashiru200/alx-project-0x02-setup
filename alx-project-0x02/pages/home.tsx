600b7e82b38ad2767f6fa52165823437196aa56d
Bashiru200 <bashirubarrie12@gmail.com>
2025-09-23 21:29:58 -0400

    creating reusable components

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,10 @@
+import Card from "@/components/common/Card";
+
 const Home : React.FC = () => {
     return (
-        <div>
-            <h1>This is the home page</h1>
+        <div className="flex gap-4 p-6 bg-teal-900 min-h-screen "> 
+            <Card title="Card Title 1" content="This is the content of the card" />
+            <Card title="Card Title 2" content="More content goes here" />
         </div>
     )
 }
