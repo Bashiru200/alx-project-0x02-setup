793a0561e374bd0dc3916019a1f9ddbe84cfbd94
Bashiru200 <bashirubarrie12@gmail.com>
2025-09-23 21:40:58 -0400

    creating reusable components

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-type CardProps = {
+
+export type CardProps = {
     title: string;
     content: string;
 };
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,15 @@
-import Card from "@/components/common/Card";
+import Card, { type CardProps } from "@/components/common/Card";
 
 const Home : React.FC = () => {
+    const cards: CardProps[] = [ 
+        { title:"Card Title 1", content:"This is the content of the card" },
+        { title:"Card Title 2", content:"More content goes here"}
+    ]
     return (
         <div className="flex gap-4 p-6 bg-teal-900 min-h-screen "> 
-            <Card title="Card Title 1" content="This is the content of the card" />
-            <Card title="Card Title 2" content="More content goes here" />
+           {cards.map((c, i) => (
+           <Card key={i} {...c} />
+           ))}
         </div>
     )
 }
